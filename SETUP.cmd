0dc29dd39dbcf7033a2f8efcf12c07771db2f282
Alejandro Colomar <Colomar.6.4.3@GMail.com>
2018-09-12 23:11:20 +0200

    Update SETUP.cmd

--- SETUP.cmd
+++ SETUP.cmd
@@ -13,39 +13,39 @@ call	"%~dp0\UNINSTALL.cmd"
 @echo	off
 
 mkdir	"%ProgramFiles(x86)%\mine-sweeper\"
-echo	"Create %ProgramFiles(x86)%/mine-sweeper/"
+echo	Create %ProgramFiles(x86)%/mine-sweeper/
 mkdir	"%ProgramFiles(x86)%\mine-sweeper\bin\"
-echo	"Create %ProgramFiles(x86)%/mine-sweeper/bin/"
+echo	Create %ProgramFiles(x86)%/mine-sweeper/bin/
 mkdir	"%ProgramFiles(x86)%\mine-sweeper\share\"
-echo	"Create %ProgramFiles(x86)%/mine-sweeper/share/"
+echo	Create %ProgramFiles(x86)%/mine-sweeper/share/
 mkdir	"%ProgramFiles(x86)%\mine-sweeper\var\"
-echo	"Create %ProgramFiles(x86)%/mine-sweeper/var/"
-echo	""
+echo	Create %ProgramFiles(x86)%/mine-sweeper/var/
+echo	
 
 copy	"%~dp0\UNINSTALL.cmd"		"%ProgramFiles(x86)%\mine-sweeper"
-echo	"Copy UNINSTALL.cmd"
+echo	Copy UNINSTALL.cmd
 copy	"%~dp0\COPYING.txt"		"%ProgramFiles(x86)%\mine-sweeper"
-echo	"Copy COPYING.txt"
+echo	Copy COPYING.txt
 copy	"%~dp0\README.txt"		"%ProgramFiles(x86)%\mine-sweeper"
-echo	"Copy README.txt"
+echo	Copy README.txt
 robocopy	"%~dp0\share"		"%ProgramFiles(x86)%\mine-sweeper\share"	/e
-echo	"Copy share/*"
+echo	Copy share/*
 robocopy	"%~dp0\var"		"%ProgramFiles(x86)%\mine-sweeper\var"		/e
-echo	"Copy var/*"
+echo	Copy var/*
 copy	"%~dp0\bin\mine-sweeper.exe"	"%ProgramFiles(x86)%\mine-sweeper\bin"
-echo	"Copy bin/mine-sweeper.exe"
-echo	""
+echo	Copy bin/mine-sweeper.exe
+echo	
 
 mkdir	"%userprofile%\Start Menu\Programs\mine-sweeper\"
 mklink	"%userprofile%\Start Menu\Programs\mine-sweeper\mine-sweeper"	"%ProgramFiles(x86)%\mine-sweeper\bin\mine-sweeper.exe"
 mklink	"%userprofile%\Start Menu\Programs\mine-sweeper\UNINSTALL"	"%ProgramFiles(x86)%\mine-sweeper\UNINSTALL.cmd"
 mklink	"%userprofile%\Start Menu\Programs\mine-sweeper\README"		"%ProgramFiles(x86)%\mine-sweeper\README.txt"
 mklink	"%userprofile%\Start Menu\Programs\mine-sweeper\COPYING"	"%ProgramFiles(x86)%\mine-sweeper\COPYING.txt"
-echo	"Create start menu shortcuts"
-echo	""
+echo	Create start menu shortcuts
+echo	
 
-echo	"Done"
-echo	""
+echo	Done
+echo	
 
 pause
 
